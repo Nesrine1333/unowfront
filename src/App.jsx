e75a02a10d4d503d6e34eb6c453749d157bdea14
nesrine ncibi <nesrine.ncibi@polytechnicien.tn>
2024-05-20 22:25:16 +0100

    preventscript

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import "./App.css";
 import { Carousel } from "./components/Carousel";
+import{useEffect} from 'react';
 
 import { slides } from "./data/carouselData.json";
 
 
+
+
+
 function App() {
+
+
+  useEffect(()=>{
+    document.addEventListener('contextmenu',handlecontextmenu)
+    return()=>{
+      document.removeEventListener('contextlenu',handlecontextmenu)
+    }
+  })
+
+  const handlecontextmenu=(e)=>{
+  e.preventDefault()
+  alert("right click not allowed")
+  }
+
   return (
     <div className="App">
       <Carousel data={slides} />
